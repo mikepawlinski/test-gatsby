ba7d4a8e55d7a03eb7cb9b0b73a711be2c28756a
Mike Pawlinski <91472483+mikepawlinski@users.noreply.github.com>
2022-06-15 15:26:46 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,7 +83,7 @@ const IndexPage = () => (
         style={{ marginBottom: `var(--space-3)` }}
       />
       <h1>
-        Bry
+        Bry 2
       </h1>
       <p className={styles.intro}>
         <b>Example pages:</b>{" "}
